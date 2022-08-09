16d761aa7eef803415f42d362a12e2ae65cae6cf
jeongbin <qls2467@gmail.com>
2022-08-09 06:11:18 -0600

    My Project

--- 01-birthday_reminder/setup/src/App.js
+++ 01-birthday_reminder/setup/src/App.js
@@ -1,8 +1,27 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [clear, setClear] = useState(false);
+  return (
+    <div className="big-box">
+      <h2>{data.length} birthdays today</h2>
+      <section>
+        {clear === true
+          ? ""
+          : data.map((item) => {
+              return (
+                <div key={item.id}>
+                  <img src={item.image} />
+                  <p>{item.name}</p>
+                  <p>{item.age}</p>
+                </div>
+              );
+            })}
+      </section>
+      <button onClick={() => setClear(true)}>Clear All</button>
+    </div>
+  );
 }
 
 export default App;
